6a219a4f73dbed38df88cca9790eee746c7f3f40
r <ronixotieno9@gmail.com>
2025-11-14 09:47:52 +0300

    Add files via upload

---
+++ For_Loop_on_numberDecrement.c
@@ -0,0 +1,17 @@
+/*
+Name:Ronix otieno
+Registration number:PA106/G/28800/25
+Description: loop on number decrement
+*/
+#include <stdio.h>
+int main()
+{
+	int i;
+	printf("Number from 100 to 50:\n");
+    for( i =100;i >=50;i--)
+	   {
+      printf("%d\n",i);
+	}
+	
+	return 0;
+}
